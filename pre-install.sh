7c79ab4245e543a2e0607dfe08d6c95f11f854d3
snowball1990 <snowballer0705@gmail.com>
2019-06-06 15:56:31 +0800

    :white_check_mark: 测试install

--- pre-install.sh
+++ pre-install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-
-echo 'check!!!'
+echo ========== CHECKING FOR CHANGES ========
+changes=$(git diff HEAD~2 HEAD -- yarn.lock)
+if [ -n "$changes" ]; then
+    echo ""
+    echo "*** CHANGES FOUND ***"
+    echo "$changes"
+    echo "Yarn.lock has changed"
+    rm -rf ./node_modules
+    yarn install
+else
+    echo "Yarn.lock has not changed"
+    yarn install
+fi
