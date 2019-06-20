eacd93312df927868f09c3fb63562df6a664a255
snowball1990 <snowballer0705@gmail.com>
2019-06-21 03:55:08 +0800

    :white_check_mark: 测试docker

--- build/auto-check.sh
+++ build/auto-check.sh
@@ -9,11 +9,10 @@ if [ -n "$changes" ]; then
     yarn install
     docker build -t node-base -f node.dockerfile .
     docker tag node-base harbor.snowball.site/web/node-base
-    docker login harbor.snowball.site
+    # docker login harbor.snowball.site
     docker push harbor.snowball.site/web/node-base
 else
     echo ""
     echo "*** CHANGES NOT FOUND ***"
     echo "Yarn.lock has not changed"
-    echo $USERNAME
 fi
