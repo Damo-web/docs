9dcde5f51acda2803a370be4aa8048dcbd6eb70a
snowball1990 <snowballer0705@gmail.com>
2019-06-29 02:17:35 +0800

    :sparkles: 新增nginx文档

--- build/auto-check-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo ========== CHECKING FOR WEB CONTAINER ========
-
-container=$(docker ps -q -f name="web-server")
-if [ -n "$container" ]; then
-  echo "Docker container web-server is existed"
-else
-  echo "Docker container web-server is not existed"
-fi
